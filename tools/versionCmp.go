c5bce2538ec4b025402490799f17848882131ae2
sakusho <sakuraishota@126.com>
2021-11-07 19:56:45 +0800

    partial

--- tools/versionCmp.go
+++ tools/versionCmp.go
@@ -1,14 +1,16 @@
 package tools
 
 import (
+	"encoding/json"
+	"fmt"
 	"log"
 	"strconv"
 	"strings"
 )
 
-func versionCmp(s1 string, s2 string, flag bool) bool {
-	arr1 := strings.Split(s1, ".")
-	arr2 := strings.Split(s2, ".")
+func VersionCmp(a string, b string) int {
+	arr1 := strings.Split(a, ".")
+	arr2 := strings.Split(b, ".")
 	for index := 0; index < len(arr1); index++ {
 		intTemp, err := strconv.Atoi(arr1[index])
 		if err != nil {
@@ -27,19 +29,41 @@ func versionCmp(s1 string, s2 string, flag bool) bool {
 	if len(arr1) < len(arr2) {
 		arrLen = len(arr1)
 	}
-	if flag {
 		for index := 0; index < arrLen; index++ {
 			if strings.Compare(arr1[index], arr2[index]) < 0 {
-				return false
+				return -1
+			}else if strings.Compare(arr1[index], arr2[index]) > 0 {
+				return 1
 			}
 		}
-		return true
-	} else {
-		for index := 0; index < arrLen; index++ {
-			if strings.Compare(arr1[index], arr2[index]) > 0 {
-				return false
-			}
-		}
-		return true
+		return 0
+}
+
+func ConvertFullRuleToJSON(rule *map[string]string, devicelist *[]string) *string{
+	mjson,_ :=json.Marshal(*rule)
+	mString :=string(mjson)
+	var mString2 string
+	mString2 += "["
+	for _, i := range *devicelist {
+		mString2 += i + ","
 	}
+	mString2 = mString2[0 : len(mString2) - 1]
+	mString2 += "]"
+	mString += "," + mString2
+	fmt.Println(mString)
+	return &mString
+}
+
+func ConvertSimplifiedRulesListToJson(rules *[]map[string]string) *string{
+	var ans string
+	ans += "["
+	for _, i := range *rules{
+		mjson,_ :=json.Marshal(i)
+		mString :=string(mjson)
+		ans += mString + ","
+	}
+	ans = ans[0 : len(ans) - 1]
+	ans += "]"
+	fmt.Println(ans)
+	return &ans
 }
