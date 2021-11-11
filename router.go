bd4ebd88f1355a443dbb6e3ee65775fb24203139
Bubbleioa <1342130847@qq.com>
2021-11-11 16:47:19 +0800

    rm public

--- router.go
+++ router.go
@@ -1,8 +1,6 @@
 package main
 
 import (
-	"techtrainingcamp-AppUpgrade/admin"
-
 	"techtrainingcamp-AppUpgrade/service"
 
 	"github.com/gin-gonic/gin"
@@ -18,13 +16,13 @@ func customizeouter(r *gin.Engine) {
 
 }
 
-func adminRouter(r *gin.Engine) {
-	//r.LoadHTMLFiles("./public/index.html")
-	r.GET("/index", admin.GetHTML)
-	r.GET("/query_all_rules", admin.QueryAllRules)
-	r.GET("/query_rule", admin.QueryRule)
-	r.POST("/update_rule", admin.UpdateRule)
-	r.POST("/create_rule", admin.CreateRule)
-	r.GET("/delete_rule", admin.DeleteRule)
-	r.GET("/disable_rule", admin.DisableRule)
-}
+// func adminRouter(r *gin.Engine) {
+// 	r.LoadHTMLFiles("./public/index.html")
+// 	r.GET("/index", admin.GetHTML)
+// 	r.GET("/query_all_rules", admin.QueryAllRules)
+// 	r.GET("/query_rule", admin.QueryRule)
+// 	r.POST("/update_rule", admin.UpdateRule)
+// 	r.POST("/create_rule", admin.CreateRule)
+// 	r.GET("/delete_rule", admin.DeleteRule)
+// 	r.GET("/disable_rule", admin.DisableRule)
+// }
