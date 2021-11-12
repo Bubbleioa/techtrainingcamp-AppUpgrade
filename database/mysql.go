705beb703eeafb1b7f5378a9d730c39b881c7252
lire <me@lirewriter.cn>
2021-11-12 21:42:19 +0800

    fix database create table

--- database/mysql.go
+++ database/mysql.go
@@ -29,9 +29,13 @@ func OpenMysql() error {
 }
 
 func MysqlCreateTable() {
+	fmt.Println("qwq")
 	OpenMysql()
 	defer db.Close()
-	db.Exec("use app; CREATE TABLE rules(id int UNSIGNED AUTO_INCREMENT,aid INT UNSIGNED,hit_count INT UNSIGNED DEFAULT 0,download_count INT UNSIGNED DEFAULT 0,  platform CHAR(16),download_url VARCHAR(128),update_version_code	VARCHAR(128),device_list TEXT,md5	VARCHAR(128),max_update_version_code	VARCHAR(128),min_update_version_code	VARCHAR(128),max_os_api	TINYINT UNSIGNED,min_os_api	TINYINT UNSIGNED,cpu_arch	TINYINT UNSIGNED,channel	VARCHAR(128),title	VARCHAR(256),update_tips	VARCHAR(1024),enabled	BOOLEAN DEFAULT true,create_date DATETIME DEFAULT CURRENT_TIMESTAMP,PRIMARY KEY ( id ));")
+	_, err := db.Exec("CREATE TABLE rules(id int UNSIGNED AUTO_INCREMENT,aid INT UNSIGNED,hit_count INT UNSIGNED DEFAULT 0,download_count INT UNSIGNED DEFAULT 0,  platform CHAR(16),download_url VARCHAR(128),update_version_code VARCHAR(128),device_list TEXT,md5 VARCHAR(128),max_update_version_code VARCHAR(128),min_update_version_code VARCHAR(128),max_os_api TINYINT UNSIGNED,min_os_api TINYINT UNSIGNED,cpu_arch TINYINT UNSIGNED,channel VARCHAR(128),title VARCHAR(256),update_tips VARCHAR(1024),enabled BOOLEAN DEFAULT true,create_date DATETIME DEFAULT CURRENT_TIMESTAMP,PRIMARY KEY ( id ));")
+	if err != nil {
+		panic(err)
+	}
 }
 
 func MysqlAddRule(rulemap *map[string]string, devicelst *[]string) (int64, error) {
